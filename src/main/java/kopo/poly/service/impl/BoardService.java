13b888dbea206c5de27a649d695f7992cc76e71f
donguk <donguk0105@naver.com>
2024-04-14 15:55:04 +0900

    주석 추가

--- src/main/java/kopo/poly/service/impl/BoardService.java
+++ src/main/java/kopo/poly/service/impl/BoardService.java
@@ -17,22 +17,34 @@ public class BoardService implements IBoardService {
 
     private final BoardRepository boardRepository;
 
+    /**
+     * 리스트 가져오기
+     */
     @Override
     public List<BoardDTO> getBoardList() throws Exception {
 
         log.info("service 보드 리스트 가져오기");
 
-
         return null;
     }
 
+    /**
+     * 게시글 상세정보 가져오기
+     * @param boardSeq PK
+     * @param type 조회수 증가여부
+     */
     @Override
     public BoardDTO getBoardInfo(Long boardSeq, Boolean type) throws Exception {
         return null;
     }
 
+    /**
+     * 게시글 업데이트
+     */
     @Override
     public int updateBoard(Long boardSeq, String userId, String title, String noticeYn, String category, String contents) throws Exception {
         return 0;
     }
+
+
 }
