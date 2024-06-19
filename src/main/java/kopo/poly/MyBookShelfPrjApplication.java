71056621bebfe9b691f4fccfea6aa6ee0f98f2cc
donguk <donguk0105@naver.com>
2024-06-19 15:34:42 +0900

    Spring Session namespace 변경

--- src/main/java/kopo/poly/MyBookShelfPrjApplication.java
+++ src/main/java/kopo/poly/MyBookShelfPrjApplication.java
@@ -2,8 +2,10 @@ package kopo.poly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession(redisNamespace = "${spring.session.redis.namespace}")
 public class MyBookShelfPrjApplication {
 
     public static void main(String[] args) {
