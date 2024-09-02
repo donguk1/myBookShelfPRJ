86407f2df88a4ff74d5d2e6b87540234987e3f01
donguk1 <donguk0105@naver.com>
2024-09-02 14:33:43 +0900

    NaverAuth 수정 KakaoAuth 추가

--- src/main/java/kopo/poly/feign/KakaoAuthFeign.java
+++ src/main/java/kopo/poly/feign/KakaoAuthFeign.java
@@ -1,32 +1,33 @@
 package kopo.poly.feign;
 
 import feign.Headers;
-import feign.RequestLine;
 import feign.Param;
+import feign.RequestLine;
 import kopo.poly.config.OpenFeignConfig;
-import kopo.poly.dto.NaverDTO;
+import kopo.poly.dto.KakaoDTO;
 import kopo.poly.dto.TokenDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "naverAuthClient", configuration = OpenFeignConfig.class)
-public interface NaverAuthFeign {
+@FeignClient(name = "KakaoAuthClient", configuration = OpenFeignConfig.class)
+public interface KakaoAuthFeign {
 
-    @RequestLine("POST https://nid.naver.com/oauth2.0/token")
+    @RequestLine("POST https://kauth.kakao.com/oauth/token")
     @Headers("Content-Type: application/x-www-form-urlencoded;charset=utf-8")
     TokenDTO getAccessToken(
             @Param("grant_type") String grantType,
             @Param("client_id") String clientId,
-            @Param("client_secret") String clientSecret,
             @Param("redirect_uri") String redirectUri,
             @Param("code") String code
     );
 
-    @RequestLine("POST https://openapi.naver.com/v1/nid/me")
+
+    @RequestLine("POST https://kapi.kakao.com/v2/user/me")
     @Headers({
             "Authorization: Bearer {accessToken}",
             "Content-Type: application/x-www-form-urlencoded;charset=utf-8"
     })
-    NaverDTO getUserInfo(
+    KakaoDTO getUserInfo(
             @Param("accessToken") String accessToken
     );
 }
+
