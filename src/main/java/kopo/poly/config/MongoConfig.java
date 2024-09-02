8d0d0102eaca1450c8b02a080f6899f3215fe982
donguk1 <donguk0105@naver.com>
2024-09-02 09:41:24 +0900

    mongo 수정

--- src/main/java/kopo/poly/config/MongoConfig.java
+++ src/main/java/kopo/poly/config/MongoConfig.java
@@ -4,11 +4,16 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
 
     @Override
     protected String getDatabaseName() {
@@ -17,7 +22,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://user:1234@cluster0.15lc2xb.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0?ssl=true");
+        ConnectionString connectionString = new ConnectionString(mongoUri);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
